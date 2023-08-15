e46fe63a55f842da95d7e17e0715d4ee874e14a4
sker <102136858+blow-sker@users.noreply.github.com>
2023-08-15 10:38:28 +0800

    Update GitTest.java

--- src/main/java/com/sker/git/GitTest.java
+++ src/main/java/com/sker/git/GitTest.java
@@ -9,7 +9,7 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
-
+        System.out.println("push22 test!");
         System.out.println("push3 test!");
 
     }
